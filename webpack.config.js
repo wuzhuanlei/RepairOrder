92302a02867c4cb26003e4077d3eecc391dba50f
Julie <wuzhuanlei@users.noreply.github.com>
2017-01-12 15:00:51 +0800

    Add files via upload
    add project files

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+// Learn more on how to config.
+// - https://github.com/ant-tool/atool-build#配置扩展
+
+'use strict';
+
+const webpack = require('webpack');
+
+module.exports = {
+    target: 'web',
+    cache: true,
+    entry: {
+        css: ['./node_modules/todomvc-app-css/index.css'],
+        common: ['classnames', 'director', 'immutable', 'reselect', 'isomorphic-fetch',
+            'react', 'react-dom', 'redux', 'react-redux', 'redux-immutable', 'redux-thunk'],
+        client: './src/client.js'
+    },
+    resolve: {
+        extensions: ["", ".webpack.js", ".web.js", ".js", ".jsx"]
+    },
+    output: {
+        path: `${__dirname}/javascripts`,
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            }
+        ]
+    },
+    plugins: [
+        // new webpack.DefinePlugin({
+        //     'process.env.NODE_ENV': '"production"'
+        // }),
+        new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
+        // new webpack.babel.plugins.push('transform-runtime'),
+        // new webpack.babel.plugins.push(['import', {
+        //     libraryName: 'antd',
+        //     style: 'css',
+        // }])
+    ]
+};
+
